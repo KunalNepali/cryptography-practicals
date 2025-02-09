341130c4538f5c65452d59d289830333cd0adc63
KunalNepali <126638830+KunalNepali@users.noreply.github.com>
2025-02-09 07:48:59 +0545

    Add files via upload

---
+++ cryptography_lab3_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    // Taking user input
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Calculating additive inverse
+    int additive_inverse = -num;
+
+    // Displaying result
+    cout << "The additive inverse of " << num << " is: " << additive_inverse << endl;
+
+    return 0;
+}
+
